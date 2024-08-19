835e3d39564d0e44fca08d111710d27c6a2579bf
Ivy1610 <8682@holbertonschool.com>
2024-08-19 15:54:12 +0200

    Async Generator

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -11,8 +11,13 @@ from typing import AsyncGenerator
 
 async def async_generator() -> AsyncGenerator[float, None]:
     """
-    That takes no arguments and
+    An async generator: that takes no arguments and
     yields 10 random numbers between 0 and 10.
+
+    Returns:
+        AsyncGenerator[float, None]: An async generator that takes
+        no arguments and yields 10 random
+        numbers between 0 and 10.
     """
     for _ in range(10):
         await asyncio.sleep(1)
