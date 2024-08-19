a6fe231d78c46e6efe7886a46273d03b74a1bbf0
Ivy1610 <8682@holbertonschool.com>
2024-08-19 15:49:39 +0200

    README

---
+++ python_async_comprehension/0-async_generator.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""
+An async generator that takes no arguments and
+yields 10 random numbers between 0 and 10.
+"""
+
+import asyncio
+import random
+from typing import AsyncGenerator
+
+
+async def async_generator() -> AsyncGenerator[float, None]:
+    """
+    That takes no arguments and
+    yields 10 random numbers between 0 and 10.
+    """
+    for _ in range(10):
+        await asyncio.sleep(1)
+        yield random.uniform(0, 10)
