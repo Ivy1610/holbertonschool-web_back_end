d83e1e3110d5709bb4ace71422668b09a53641c9
Ivy78 <mendesev77@gmail.com>
2024-12-04 14:59:10 +0100

    ajouter 3-to_str.py avec des anotation simple de la fonction to_str

---
+++ python_variable_annotations/3-to_str.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""
+3-to_str.py
+
+This module contains a type-annotated function `to_str`
+that convers a float to string
+"""
+
+
+def to_str(n: float) -> str:
+    """
+    Converts a float to its string representation
+
+    Args:
+        n (float): Float number
+
+    Returns:
+        str: String representation of n
+    """
+    return str(n)
