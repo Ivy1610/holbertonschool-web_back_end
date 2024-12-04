570fc3405cdeb19af42b14264823062a4a5a2cea
Ivy78 <mendesev77@gmail.com>
2024-12-04 13:56:28 +0100

    ajouter README et 0-add.py simple annotations pour la fonction addititionner

---
+++ python_variable_annotations/0-add.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+"""
+0-add.py
+
+This module contains a type-annotated function
+`add` that sums two floats
+"""
+
+
+def add(a: float, b: float) -> float:
+    """
+    Returns the sum of two float numbers
+
+    Args:
+        a (float): First number
+        b (float): Secon number
+
+    Returns:
+        float: Sum of a and b
+    """
+    return a + b
