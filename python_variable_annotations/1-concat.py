a5f8e3f1fff49bb35a1d7a16a82c7472e90b7af8
Ivy78 <mendesev77@gmail.com>
2024-12-04 14:32:25 +0100

    ajouter 1-concat.py pour la fonction concatener

---
+++ python_variable_annotations/1-concat.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+"""
+1-concat.py
+
+This module contains a type-annotated function `concat`
+that concatenates two strings
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """
+    Concatenates stwo string and returns the result
+
+    Args:
+        str1 (str): First string
+        str2 (str): Second string
+
+    Returns:
+        str: Concatenated string
+    """
+    return str1 + str2
