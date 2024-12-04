a1d95e67dcbd56139f66c808acc684073bad90be
Ivy78 <mendesev77@gmail.com>
2024-12-04 15:14:30 +0100

    ajouter 4-define_variables.py avec une annotation simple des differentes fonctions

---
+++ python_variable_annotations/4-define_variables.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+4-define_variables.py
+
+This module defines and annotated vaiables: a, pi,
+i_understand_annotations and school with specific values
+"""
+
+
+a: int = 1
+pi: float = 3.14
+i_understand_annotations: bool = True
+school: str = "holberton"
