c74b0c8fd52eb04eaa1ca22516305011e78c6bb9
Ivy78 <mendesev77@gmail.com>
2024-12-10 16:34:33 +0100

    The coroutine will loop 10 times, each time asynchronously wait 1 second, then yield a random number between 0 and 10.

---
+++ python_async_comprehension/0-async_generator.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+"""
+0-async_generator.py
+
+Ce module contient la coroutine `async_generator` qui génère
+des nombres aléatoires
+"""
+
+
+import asyncio
+import random
+from typing import AsyncGenerator
+
+
+async def async_generator() -> AsyncGenerator[float, None]:
+    """
+    coroutine qui génère 10 nombres aléatoires entre 0 et 10,
+    avec une pause de 1 seconde entre chaque generation
+
+    Returns:
+        AsyncGenerator[float, None]: Générateur
+        asynchrone de nombres aléatoires
+    """
+    for _ in range(10):
+        await asyncio.sleep(1)
+        yield random.uniform(0, 10)
